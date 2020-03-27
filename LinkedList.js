d65082911eb390cb7e05e4abebfc991bd30fb070
salman <salman@kutumbita.com>
2020-03-27 21:59:35 +0600

    linked list addToHead and addToTail

---
+++ LinkedList.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// linked list constructor function
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+// Node constructor function
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value, this.head, null);
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+};
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+};
