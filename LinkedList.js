feab707618e13a209c03d707ab4fe9a96f33603d
salman <salman@kutumbita.com>
2020-03-28 00:43:40 +0600

    remove head, tail, search, indexOf method

--- LinkedList.js
+++ LinkedList.js
@@ -13,6 +13,7 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 
+// Add head method
 LinkedList.prototype.addToHead = function(value) {
   var newNode = new Node(value, this.head, null);
   if (this.head) {
@@ -23,6 +24,7 @@ LinkedList.prototype.addToHead = function(value) {
   this.head = newNode;
 };
 
+// Add tail method
 LinkedList.prototype.addToTail = function(value) {
   var newNode = new Node(value, null, this.tail);
   if (this.tail) {
@@ -31,4 +33,48 @@ LinkedList.prototype.addToTail = function(value) {
     this.head = newNode;
   }
   this.tail = newNode;
+};
+
+// Remove head method
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) return null;
+  var val = this.head.value;
+  this.head = this.head.next;
+  if (this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
+};
+
+// Remove tail method
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null;
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) this.tail.next = null;
+  else this.head = null;
+  return val;
+};
+
+// Search method
+LinkedList.prototype.search = function(searchValue) {
+  var currentNode = this.head;
+  while (currentNode) {
+    if (currentNode === searchValue) return currentNode.value;
+    currentNode = currentNode.next;
+  }
+  return null;
+};
+
+// IndexOf method
+LinkedList.prototype.indexOf = function() {
+  var indexes = [];
+  var currentIndex = 0;
+  var currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === value) {
+      indexes.push(currentIndex);
+    }
+    currentNode = currentNode.next;
+    currentIndex++;
+  }
 };
